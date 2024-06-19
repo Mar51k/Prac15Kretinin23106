f0f7c67c59d2617bce6fa461779cf4080997b79f
Mar51k <dima.kretinin.05@mail.ru>
2024-06-19 19:09:27 +0700

    Prac15Kretinin23106

---
+++ app/src/main/java/com/example/Prac15Kretinin23106/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.Prac15Kretinin23106;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Получимссылкунасолнце
+        ImageView sunImageView = findViewById(R.id.sun);
+        // Анимациядлявосходасолнца
+        Animation sunRiseAnimation = AnimationUtils.loadAnimation(this, R.anim.sun_rise);
+        // Подключаеманимациюкнужному View
+        sunImageView.startAnimation(sunRiseAnimation);
+
+        ImageView clockImageView = findViewById(R.id.clock);
+// анимациядлявращениячасов
+        Animation clockTurnAnimation = AnimationUtils.loadAnimation(this, R.anim.clock_turn);
+        clockImageView.startAnimation(clockTurnAnimation);
+
+        ImageView hourImageView = findViewById(R.id.hour_hand);
+// анимациядлястрелки
+        Animation hourTurnAnimation = AnimationUtils.loadAnimation(this, R.anim.hour_turn);
+// присоединяеманимацию
+        hourImageView.startAnimation(hourTurnAnimation);
+
+
+    }
+
+}
